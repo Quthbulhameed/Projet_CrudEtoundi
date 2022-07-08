9c3f3f6188b7aad2cef149b0061d52bb021696b4
hameed20 <quthbulhameed.djiavoudine@monlycee.net>
2022-07-08 15:18:40 +0200

    Crud projet

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+$dsn = 'mysql:host=localhost:3306;dbname=company';
+$username = 'root';
+$password = 'root';
+$options = [];
+try {
+$connection = new PDO($dsn, $username, $password, $options);
+} catch(PDOException $e) {
+
+}
